1e096d24260bb81e9deb221bc8398a7588f32afa
firuz <firuz2004001@mail.ru>
2024-11-27 11:05:35 +1000

    add comments

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ProjectForTesting
     {
         static void Main(string[] args)
         {
+            /// Метод игры 4 на 4
             Console.WriteLine("Игра 4 на 4");
 
             string baseDir = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
@@ -74,22 +75,28 @@ namespace ProjectForTesting
 
         static int isUser(List<String> elems, string elem)
         {
+            /// вывод позиции пользователя в списке
             return elems.IndexOf(elem);
         }
 
         static string GetInfo(string text)
         {
+            /// Получение информации от пользователя
+            /// Считыванием данных из консоли
             Console.Write(text + " ");
             return Console.ReadLine();
         }
 
         static string GenerateNumberToString(int left, int right, Random rnd)
         {
+            /// Генерация рандомных чисел
             return rnd.Next(left, right).ToString();
         }
 
         static int MakeTop(int num, List<int> results)
         {
+            /// Функция составляет топ
+            /// определяющий текущее положение
             int current_pos = results.IndexOf(num);
 
             if (current_pos <= 5)
@@ -111,22 +118,34 @@ namespace ProjectForTesting
 
         static List<String> Read(string filename)
         {
+            /// Считывание из файла чисел
+            /// из массива номеров
             List<String> elems = new List<String>();
-            using (FileStream fstream = File.OpenRead(filename))
+            try
             {
-                byte[] buffer = new byte[fstream.Length];
-                fstream.Read(buffer, 0, buffer.Length);
+                using (FileStream fstream = File.OpenRead(filename))
+                {
+                    byte[] buffer = new byte[fstream.Length];
+                    fstream.Read(buffer, 0, buffer.Length);
 
-                string textFromFile = Encoding.Default.GetString(buffer);
+                    string textFromFile = Encoding.Default.GetString(buffer);
 
-                elems.Add(textFromFile);
+                    elems.Add(textFromFile);
+                }
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
             return elems;
         }
 
 
         static int ReadResult(string compinedPath, out Dictionary<string, List<int>> userResults, out SortedSet<int> nums)
         {
+            /// Считывание из файла результатов
+            /// И дальнейшая сортировка
             userResults = new Dictionary<string, List<int>>();
             nums = new SortedSet<int>();
             using (FileStream fstream = File.OpenRead(compinedPath))
@@ -159,6 +178,7 @@ namespace ProjectForTesting
 
         static bool WriteResult(int stepCounter, string login, string combinedPath)
         {
+            /// Запись результата игры, привязанного к пользователю
             try
             {
                 using (FileStream fstream = new FileStream(combinedPath, FileMode.Append))
@@ -169,7 +189,7 @@ namespace ProjectForTesting
                 }
             }
 
-            catch
+            catch (FileNotFoundException e)
             {
                 return false;
             }
@@ -179,6 +199,7 @@ namespace ProjectForTesting
 
         static string ReadFourDigitNumFromKeyboard(string stroka)
         {
+            /// Считывание числа из 4 чисел
             Console.Write(stroka);
             string guess = "";
             try
@@ -190,7 +211,7 @@ namespace ProjectForTesting
                 }
                 Convert.ToInt32(guess);
             }
-            catch
+            catch (Exception e)
             {
                 guess = ReadFourDigitNumFromKeyboard("Вы ввели неверное число.\nВведите число: ");
             }
@@ -199,6 +220,8 @@ namespace ProjectForTesting
 
         static bool CheckCorrect(string baseNum, string numForCheck, out int amountOfNumIncluded, out int positionAmount)
         {
+            /// Проверка введённого числа
+            /// С отгадываемым
             amountOfNumIncluded = 0;
             positionAmount = 0;
 
